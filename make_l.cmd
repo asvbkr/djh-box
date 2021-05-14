d3bd93673b1a9e2f4cfd15b28546b8554f9f7889
asvbkr <asv@bk.ru>
2021-05-14 22:23:41 +0300

    Апгрейд либов

--- make_l.cmd
+++ make_l.cmd
@@ -2,9 +2,11 @@ echo Make localization
 cd TtBot
 python ../manage.py makemessages -l ru
 python ../manage.py makemessages -l en
-python ../manage.py compilemessages
+
 cd ../TamTamBotDj
 python ../manage.py makemessages -l ru
 python ../manage.py makemessages -l en
+
+cd ../djb
 python ../manage.py compilemessages
 cd ..
